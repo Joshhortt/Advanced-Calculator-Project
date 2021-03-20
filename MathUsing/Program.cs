0dcc5d9d73a62cb1b0a1dbce954f7d913ab4b56c
Joshhortt <joshhortt@yahoo.com>
2021-03-20 18:54:09 +0000

    Add Project in Winforms 'Advanced Calculator'.

---
+++ MathUsing/Program.cs
@@ -0,0 +1,21 @@
+﻿//Advanced Calculator project is a desktop application which is developed in C# .NET. 
+//In this Project we can add two numbers and perform various operations on it.
+//Like square of two numbers, square root of two numbers, cos, sin, tan and other types of advanced operations.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathUsing
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
